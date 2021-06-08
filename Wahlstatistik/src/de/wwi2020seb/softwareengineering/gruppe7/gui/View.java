b9cd207d69aaea2436bc2b21888380483d1a7a73
Mildem122 <marcel.milde@outlook.de>
2021-06-08 14:26:52 +0200

    Update View.java

--- Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/gui/View.java
+++ Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/gui/View.java
@@ -3,18 +3,35 @@ package de.wwi2020seb.softwareengineering.gruppe7.gui;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
 
 public class View extends JFrame {
 
 	private static final long serialVersionUID = -7140986200488666516L;
 	private JPanel contentPane;
+	private JTable table;
 
 	public View(Controller controller) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(133, 117, 200, 57);
+		contentPane.add(textPane);
+		
+		table = new JTable();
+		table.setBounds(44, 25, 293, 88);
+		contentPane.add(table);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(407, 117, -29, 88);
+		contentPane.add(scrollPane);
 	}
 }
