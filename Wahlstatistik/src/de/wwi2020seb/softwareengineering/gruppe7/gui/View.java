4a1ff4d4347e296f38223dcae32c11b9f350a9d2
Mildem122 <marcel.milde@outlook.de>
2021-06-08 14:28:55 +0200

    Update View.java

--- Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/gui/View.java
+++ Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/gui/View.java
@@ -12,7 +12,6 @@ public class View extends JFrame {
 
 	private static final long serialVersionUID = -7140986200488666516L;
 	private JPanel contentPane;
-	private JTable table;
 
 	public View(Controller controller) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,17 +20,5 @@ public class View extends JFrame {
 		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(133, 117, 200, 57);
-		contentPane.add(textPane);
-		
-		table = new JTable();
-		table.setBounds(44, 25, 293, 88);
-		contentPane.add(table);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(407, 117, -29, 88);
-		contentPane.add(scrollPane);
 	}
 }
