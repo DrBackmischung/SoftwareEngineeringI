bd0b02fba1a1e920c4b11f46fd126056bfb6472f
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-06-09 21:40:45 +0200

    Update Data Model

--- Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/application/VotingCalculator.java
+++ Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/application/VotingCalculator.java
@@ -1,16 +1,34 @@
 package de.wwi2020seb.softwareengineering.gruppe7.application;
 
+import java.util.ArrayList;
+
 public class VotingCalculator {
 /*
  * Bekommt alle DistrictResultLists und fügt alle fehlenden Personen hinzu
  */
 	
-	public void enrichCandidates(ResultList d, String[] name) {
-		
+	public void enrichCandidates(ResultList d, ArrayList<String> names) {
+		for(String s : names) {
+			boolean containsName = false;
+			for(ResultMap m : d.getResults()) {
+				if(m.getName().equalsIgnoreCase(s))
+						containsName = true;
+			}
+			if(!containsName) {
+				d.addResult(new ResultMap(s, 0));
+			}
+		}
 	}
 	
 	public void calculatePercentages(ResultList d) {
-		
+		double votes = 0;
+		for(ResultMap m : d.getResults()) {
+			votes += m.getVoteCount();
+		}
+		for(ResultMap m: d.getResults() ) {
+			double result = m.getVoteCount();
+			m.setPercentage(result / votes * 100);
+		}
 	}
 	
 }
