ef5ebea7879abd7f947fdcaf6c1a62c83231ab3a
Mildem122 <marcel.milde@outlook.de>
2021-06-09 16:09:17 +0200

    ItryMyBest

--- Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/data/CSVReader.java
+++ Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/data/CSVReader.java
@@ -4,22 +4,41 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CSVReader {
 
 	public static void main(String[] args) {
 		
-		String path = "C:\\Users\\marce\\Desktop\\Mappe1.csv";
+		BufferedReader br; 
+		String paths[] = new String[2];
+		String line;
+		String values[];
+		ArrayList<String> personenList = new ArrayList<>();
 		
-		String line = "";
-		
+//Alle CSV Dateien eintragen. Bessere Lösung?
+		paths[0]  = "C:\\Users\\marce\\Desktop\\Niederfeld.csv";
+		paths[1]  = "C:\\Users\\marce\\Desktop\\Jungbusch.csv";
+
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(path));
+			for(String path : paths) {
+				
+				br = new BufferedReader(new FileReader(path));
+				while( (line = br.readLine()) != null) {
+					values = line.split(";");
+					personenList.add(values[0]);
+				}
 			
-			while( (line = br.readLine()) != null) {
-				String[] values = line.split(";");
-				System.out.println(values[1]);
+			Iterator personenIterator = personenList.iterator();
+			while(personenIterator.hasNext()) {
+				String person = (String) personenIterator.next();
+				System.out.println(person);
 			}
+			System.out.println("Finish");
+			personenList = new ArrayList<>();
+			}
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}  catch (IOException e) {
