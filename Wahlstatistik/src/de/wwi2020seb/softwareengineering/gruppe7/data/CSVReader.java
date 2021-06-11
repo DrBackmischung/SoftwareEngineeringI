86fbf18efc5e51aea77657ce57ce50074b490c18
Mildem122 <77717082+Mildem122@users.noreply.github.com>
2021-06-11 11:50:22 +0200

    Update CSVReader.java
    Better Update

--- Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/data/CSVReader.java
+++ Wahlstatistik/src/de/wwi2020seb/softwareengineering/gruppe7/data/CSVReader.java
@@ -6,8 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Set;
 
 import de.wwi2020seb.softwareengineering.gruppe7.application.ResultList;
 import de.wwi2020seb.softwareengineering.gruppe7.application.ResultMap;
@@ -28,6 +26,7 @@ public class CSVReader {
 //		fileArray[] = f.listFiles();
 		f = new File("C:\\Users\\Marcel\\Desktop\\Wahlen");
 		fileArray = f.listFiles();
+		
 		
 		AllResultLists = new ArrayList<>();
 		try {
@@ -53,58 +52,17 @@ public class CSVReader {
 			e.printStackTrace();
 			System.out.println("Probleme beim lesen der Datei. Stellen sie sicher, dass nur CSV Dateien gelesen werden");
 		}
+			finally {
+				try {
+					br.close();
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
 		
 		return AllResultLists;
 		
 				}
 
-	public static void main(String[] args) {
-		
-//		File f = new File("C:\\Users\\marce\\Desktop\\Wahlen");
-//		File fileArray[] = f.listFiles();
-		File f = new File("C:\\Users\\Marcel\\Desktop\\Wahlen");
-		File fileArray[] = f.listFiles();
-		
-		BufferedReader br;
-		String line;
-		String values[];
-		ArrayList<ResultList> AllResultLists = new ArrayList<>();
-		String districtName;
-
-		try {
-			for(File path : fileArray) {
-				values 		 = path.getName().split(".csv");
-				districtName = values[0];
-				ResultList currentDistrict = new ResultList(districtName);
-				br = new BufferedReader(new FileReader(path));
-				
-				while( (line = br.readLine()) != null) {
-					values    = line.split(";");
-				    ResultMap currentResultMap = new ResultMap(values[0], Integer.parseInt(values[1]));
-				    currentDistrict.addResult(currentResultMap);
-				}
-			
-				AllResultLists.add(currentDistrict);
-			}
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}  catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		Iterator testIterator = AllResultLists.iterator();
-		while(testIterator.hasNext()) {
-			ResultList nextResultList = (ResultList)testIterator.next();
-			System.out.println("Bezirk: " + nextResultList.getName());
-			Set resultMaps = nextResultList.getResults();
-			Iterator setIterator = resultMaps.iterator();
-				while(setIterator.hasNext()) {
-					ResultMap currentResultMap = (ResultMap)setIterator.next();
-					System.out.println(currentResultMap.getName() + " hat " + currentResultMap.getVoteCount() + " Stimmen.");
-				}
-		}
-		
-
-	}
 }
