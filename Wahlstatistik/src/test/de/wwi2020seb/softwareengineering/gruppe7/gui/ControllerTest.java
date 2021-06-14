e13a7a53515c6e56c89287f75b171fc9070258f8
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-06-14 13:08:26 +0200

    Update Controller Test

--- Wahlstatistik/src/test/de/wwi2020seb/softwareengineering/gruppe7/gui/ControllerTest.java
+++ Wahlstatistik/src/test/de/wwi2020seb/softwareengineering/gruppe7/gui/ControllerTest.java
@@ -3,12 +3,31 @@ package test.de.wwi2020seb.softwareengineering.gruppe7.gui;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import de.wwi2020seb.softwareengineering.gruppe7.gui.Controller;
-import de.wwi2020seb.softwareengineering.gruppe7.gui.Model;
 
 public class ControllerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
 	
 	@Test
 	@DisplayName("Test Singelton")
